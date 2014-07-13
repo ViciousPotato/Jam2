03d7085e55708e6f40b44c3c1485bffb8118ac46
Zhou Honglin <zhou.honglin@gmail.com>
2014-07-13 14:52:52 +0800

    Add following scenes

---
+++ Jam2/Scenes/Scene18.swift
@@ -0,0 +1,9 @@
+//
+//  Scene4.swift
+//  Jam2
+//
+//  Created by zhou on 14-7-13.
+//  Copyright (c) 2014年 zhou. All rights reserved.
+//
+
+import Foundation
---
+++ Jam2/Scenes/Scene5.swift
@@ -0,0 +1,16 @@
+//
+//  Scene4.swift
+//  Jam2
+//
+//  Created by zhou on 14-7-13.
+//  Copyright (c) 2014年 zhou. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Scene5 : BaseScene {
+  override func didMoveToView(view: SKView) {
+    
+  }
+}
